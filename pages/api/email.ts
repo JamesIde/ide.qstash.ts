d7c808f97a689451ebb5c3a828f56cbd78077733
James <james.ide775@gmail.com>
2023-01-28 21:02:48 +1030

    Fix route

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -3,18 +3,13 @@ import { verifySignature } from "@upstash/qstash/nextjs";
 import nodeMailerClient from "@/lib/nodemailerClient";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { method } = req;
-  if (method === "POST") {
-    await nodeMailerClient().sendMail({
-      from: process.env.SMPT_USERNAME,
-      to: process.env.ADMIN_EMAIL,
-      subject: "This is a test",
-      html: "<p>This email was sent from ide.broker.ts on the back of a message received from Qstash broker.</p>",
-    });
-    res.status(200).json({ ok: true });
-  } else {
-    res.status(405).json({ ok: false });
-  }
+  await nodeMailerClient().sendMail({
+    from: process.env.SMPT_USERNAME,
+    to: process.env.ADMIN_EMAIL,
+    subject: "This is a test",
+    html: "<p>This email was sent from ide.broker.ts on the back of a message received from Qstash broker.</p>",
+  });
+  res.status(200).json({ ok: true });
 }
 
 export const config = {
