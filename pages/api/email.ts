c0a4a079e0af176693d900f7255a18afdbbbc8eb
James <james.ide775@gmail.com>
2023-01-28 20:57:34 +1030

    Fix

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -3,14 +3,19 @@ import { verifySignature } from "@upstash/qstash/nextjs";
 import nodeMailerClient from "@/lib/nodemailerClient";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  await nodeMailerClient().sendMail({
-    from: process.env.SMPT_USERNAME,
-    to: process.env.ADMIN_EMAIL,
-    subject: "This is a test",
-    html: "<p>This email was sent from ide.broker.ts on the back of a message received from Qstash broker.</p>",
-  });
+  const { method } = req;
+  if (method === "POST") {
+    await nodeMailerClient().sendMail({
+      from: process.env.SMPT_USERNAME,
+      to: process.env.ADMIN_EMAIL,
+      subject: "This is a test",
+      html: "<p>This email was sent from ide.broker.ts on the back of a message received from Qstash broker.</p>",
+    });
 
-  res.status(200).json({ ok: true });
+    res.status(200).json({ ok: true });
+  } else {
+    res.status(405).json({ ok: false });
+  }
 }
 
 // Verifies the JWT signature that comes from the webhook
