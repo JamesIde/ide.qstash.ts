2b6af00ae22ba0f2f9181bcbdccb0fad5728ac7d
James <james.ide775@gmail.com>
2023-01-29 07:45:57 +1030

    Email

--- pages/api/qstash.ts
+++ pages/api/qstash.ts
@@ -9,10 +9,10 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     subject: "This is a test",
     html: "<p>This email was sent from ide.broker.ts on the back of a message received from Qstash broker.</p>",
   });
-  res.status(200).json({ ok: true });
+
+  res.status(200).end();
 }
 
-// Verifies the JWT signature that comes from the webhook
 export default verifySignature(handler);
 
 export const config = {
