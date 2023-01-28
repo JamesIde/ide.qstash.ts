a77687602c9b3098cdc6f21e6e6c7a988d39f9bc
James <james.ide775@gmail.com>
2023-01-28 21:00:55 +1030

    Body parser to fix blocked messages

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -11,12 +11,16 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       subject: "This is a test",
       html: "<p>This email was sent from ide.broker.ts on the back of a message received from Qstash broker.</p>",
     });
-
     res.status(200).json({ ok: true });
   } else {
     res.status(405).json({ ok: false });
   }
 }
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 // Verifies the JWT signature that comes from the webhook
 export default verifySignature(handler);
